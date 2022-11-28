586b26ed910beff7d34a808f97a1550a02397852
nathan-assefa <nnathanassefa@gmail.com>
2022-11-28 06:27:31 -0800

    Checking if there is loop in a linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
-	while (slowi != NULL && fast != NULL && fast->next != NULL)
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
