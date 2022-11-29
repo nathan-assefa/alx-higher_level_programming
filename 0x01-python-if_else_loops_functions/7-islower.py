2dd94194274c9ca6dd67131e9931cb1185066fb7
nathan-assefa <nnathanassefa@gmail.com>
2022-11-29 10:30:50 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
-# islower = __import__('7-islower').islower
-
 def islower(c):
-    for lower in range(ord("a"), ord("z") + 1):
-        if (c == chr(lower)):
-            return (True)
-    return (False)
-
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+    if c >= 'a' and c <= 'z':
+        return True
+    else:
+        return False
