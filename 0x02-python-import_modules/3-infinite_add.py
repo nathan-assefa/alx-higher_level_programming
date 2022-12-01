5f09c91932c48c88a8d8816d207c6c3cf772ae88
nathan-assefa <nnathanassefa@gmail.com>
2022-12-01 06:29:57 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 from sys import argv
 
-def main():
+if __name__ == "__main__":
     _sum = 0;
     i = 1
     while (i < len(argv)):
         _sum += int(argv[i])
         i += 1
 
-    print(_sum)
-if __name__ == "__main__":
-    main()
+    print("{}".format(_sum))
