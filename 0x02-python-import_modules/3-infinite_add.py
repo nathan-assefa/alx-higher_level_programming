defa391bcb95599872daaf8cd04c91e9d27cc87c
nathan-assefa <nnathanassefa@gmail.com>
2022-12-01 03:06:02 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,10 @@
 from sys import argv
 
 i = 1
-if len(argv) != 1:
-    while (i < len(argv)):
-        print("{}".format(int(argv[i])))
-        i += 1
+def main():
+    if len(argv) != 1:
+        while (i < len(argv)):
+            print("{}".format(int(argv[i])))
+            i += 1
+if __name__ == "__main__":
+    main()
