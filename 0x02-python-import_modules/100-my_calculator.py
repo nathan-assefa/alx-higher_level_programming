e060644550b52fc538532e2d32e1203af9a41394
nathan-assefa <nnathanassefa@gmail.com>
2022-12-01 07:14:09 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,11 @@ from calculator_1 import add, sub, mul, div
 operator = ['+', '-', '*', '/']
 
 if __name__ == "__main__":
-    if len(argv) != 4:
+    if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif argv[2] not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], add(int(argv[1]), int(argv[3]))))
+        print("{} + {} = {}".format(argv[1], argv[2], argv[3], add(int(argv[1]), int(argv[3]))))
