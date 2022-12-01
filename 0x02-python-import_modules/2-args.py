a07c45965c4fbc35d4c20533c8108c9042d3a420
nathan-assefa <nnathanassefa@gmail.com>
2022-12-01 05:16:24 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
+def main():
+    if len(argv) == 2:
+        print("{} {}:".format(len(argv) - 1, "argument"))
+        print("{}: {}".format(len(argv) - 1, argv[1]))
+    elif len(argv) == 1:
+        print("{} {}.".format(len(argv) - 1, "arguments"))
+    else:
+        print("{} {}:".format(len(argv) - 1, "arguments"))
 
-if len(argv) == 2:
-    print("{} {}:".format(len(argv) - 1, "argument"))
-    print("{}: {}".format(len(argv) - 1, argv[1]))
-elif len(argv) == 1:
-    print("{} {}.".format(len(argv) - 1, "arguments"))
-else:
-    print("{} {}:".format(len(argv) - 1, "arguments"))
-
-    i = 1
-    while (i < len(argv)):
-        print("{}: {}".format(i, argv[i]))
-        i += 1
+        i = 1
+        while (i < len(argv)):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
+if __name__ == "__main__":
+    main()
