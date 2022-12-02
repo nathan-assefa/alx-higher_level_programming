249c5973b53000a47e8029ba638c6d927681e382
nathan-assefa <nnathanassefa@gmail.com>
2022-12-02 02:02:43 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     elif argv[2] not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    else:
-        a = int(argv[1])
-        b = int(argv[3])
-        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], operator[argv[2]](a, b)))
-
+    a = int(argv[1])
+    b = int(argv[3])
+    print(
+        "{} {} {} = {}"
+        .format(argv[1], argv[2], argv[3], operator[argv[2]](a, b))
+    )
