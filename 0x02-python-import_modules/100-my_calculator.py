8ce27410b020452732b6725a48c00022816a7cb8
nathan-assefa <nnathanassefa@gmail.com>
2022-12-02 02:13:22 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,12 +8,13 @@ if __name__ == "__main__":
     if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] not in operator:
+    elif argv[2] not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    print(
-        "{} {} {} = {}"
-        .format(argv[1], argv[2], argv[3], operator[argv[2]](a, b))
-    )
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        print(
+            "{} {} {} = {}"
+            .format(argv[1], argv[2], argv[3], operator[argv[2]](a, b))
+        )
