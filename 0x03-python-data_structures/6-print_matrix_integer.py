d528ef3aa691a07dc3d705f08e7e123e7b61c216
nathan-assefa <nnathanassefa@gmail.com>
2022-12-02 20:59:26 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1:
-        print('')
-    else:
-        for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                print(matrix[i][j], end=" ")
-            print()
+if __name__ = "__main__":
+    def print_matrix_integer(matrix=[[]]):
+        if len(matrix) == 1:
+            print('')
+        else:
+            for i in range(len(matrix)):
+                for j in range(len(matrix)):
+                    print(matrix[i][j], end=" ")
+                print()
