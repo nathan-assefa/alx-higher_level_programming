d99ab15a3e081ef857a679da9ece9136c8e8d5ab
nathan-assefa <nnathanassefa@gmail.com>
2022-12-05 09:55:09 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,5 +5,5 @@ def new_in_list(my_list, idx, element):
     new_list = []
     for item in my_list:
         new_list.append(item)
-        new_list[idx] = element
-        return new_list
+    new_list[idx] = element
+    return new_list
