39af0f01049fd14ce6f53b6054c9b60945b2edf6
nathan-assefa <nnathanassefa@gmail.com>
2022-12-07 12:14:44 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-
-    new_set = list()
-    for st1 in set_1:
-        for st2 in set_2:
-            if st1 != st2:
-                new_set.append(st1)
-                new_set.append(st2)
-    return set(new_set)
+    inSet1 = set_1 - set_2
+    inSet2 = set_2 - set_1
+    new_set = inSet1.union(inSet2)
+    return new_set
