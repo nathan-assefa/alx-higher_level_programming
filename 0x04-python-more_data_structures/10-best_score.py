c3d92f17135cedb48ac80af74ec9e275bc6e4900
nathan-assefa <nnathanassefa@gmail.com>
2022-12-07 12:28:38 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
 
-    new_ma_val = max(a_dictionary, key= lambda x: a_dictionary[x])
+    new_ma_val = max(a_dictionary, key=lambda x: a_dictionary[x])
     return new_ma_val
