0bf55b6217c27bfcfdad84432e60bebd5d6a5a59
nathan-assefa <nnathanassefa@gmail.com>
2022-12-07 12:41:14 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,50 +1,40 @@
 #!/usr/bin/python3
-def value(r):
-    if (r == 'I'):
-        return 1
-    if (r == 'V'):
-        return 5
-    if (r == 'X'):
-        return 10
-    if (r == 'L'):
-        return 50
-    if (r == 'C'):
-        return 100
-    if (r == 'D'):
-        return 500
-    if (r == 'M'):
-        return 1000
-    return -1
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
 
 def roman_to_int(roman_string):
-    res = 0
-    i = 0
+        if not roman_string:
+            return 0
 
-    while (i < len(str)):
+        if not isinstance(roman_string, str):
+            return 0
 
-        # Getting value of symbol s[i]
-        s1 = value(str[i])
+        rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        list_keys = list(rom_n.keys())
 
-        if (i + 1 < len(str)):
+        num = 0
+        last_rom = 0
+        list_num = [0]
 
-            # Getting value of symbol s[i + 1]
-            s2 = value(str[i + 1])
+        for ch in roman_string:
+            for r_num in list_keys:
+                if r_num == ch:
+                    if rom_n.get(ch) <= last_rom:
+                        num += to_subtract(list_num)
+                        list_num = [rom_n.get(ch)]
+                    else:
+                        list_num.append(rom_n.get(ch))
 
-            # Comparing both values
-            if (s1 >= s2):
+                        last_rom = rom_n.get(ch)
 
-                # Value of current symbol is greater
-                # or equal to the next symbol
-                res = res + s1
-                i = i + 1
-            else:
+        num += to_subtract(list_num)
 
-                # Value of current symbol is greater
-                # or equal to the next symbol
-                res = res + s2 - s1
-                i = i + 2
-            else:
-                res = res + s1
-                i = i + 1
-
-    return res
+        return (num)
