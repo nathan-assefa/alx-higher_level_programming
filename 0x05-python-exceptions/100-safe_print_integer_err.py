73d9a90896eeca3ac9f1cf5670961669ed1860ce
nathan-assefa <nnathanassefa@gmail.com>
2022-12-19 12:41:58 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-try:
-    print("{:d}".format(value))
-    return (True)
-except (TypeError, ValueError):
-    print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-    return (False)
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
