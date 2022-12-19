a3d0d1c51f841858b021cb03ae7eed29bbbcd5f2
nathan-assefa <nnathanassefa@gmail.com>
2022-12-19 03:05:30 -0800

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for element in range(x):
+        try:
+            print(my_list[element], end="")
+            i += 1
+        except IndexError:
+            break
+        print()
+        return i
