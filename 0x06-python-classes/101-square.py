806b7ba2485cd512b847a1d41db996028565be1d
nathan-assefa <nnathanassefa@gmail.com>
2022-12-21 23:55:50 -0800

    0x06-python-classes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -47,11 +47,9 @@ class Square:
 
     def my_print(self):
         """Print the square with the # character."""
-        if self.__size == 0:
-            print("")
-            return
 
-        [print("") for i in range(0, self.__position[1])]
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
