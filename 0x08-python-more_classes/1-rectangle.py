5d3a0d307a2a01c851bbef35c133b5971d98589d
nathan-assefa <nnathanassefa@gmail.com>
2023-01-02 02:32:16 -0800

    0x08-python-more_classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""defining a class"""
+
+
+class Rectangle:
+
+    """Initializing a new object"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """Getting width attribute"""
+    @property
+    def width(self):
+        return self.__width
+
+    """Setting widht attribute"""
+    @width.setter
+    def width(self, value):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+    """getting height attribute"""
+    @property
+    def height(self):
+        return self.__height
+
+    """setting height attribute"""
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
