69e17cb3af4785c600d90152419d917485b2a5ad
nathan-assefa <nnathanassefa@gmail.com>
2023-01-03 06:02:06 -0800

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""This class cleans input"""
+"""Defines the LockedClass class."""
 
 
 class LockedClass:
+    """
+    Prevents the user from dynamically creating new instance attributes.
+    """
+
     __slots__ = ["first_name"]
