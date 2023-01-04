e2a24c63d25a832296f02a596be20083f0b4c874
nathan-assefa <nnathanassefa@gmail.com>
2023-01-04 09:45:06 -0800

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,6 +6,12 @@ function performs the operation on
 
 
 def add_integer(a, b=98):
+    """Add two integer
+    Arg:
+        a = first paramter to be added
+        b = second parameter to be added
+    paramter b has defalut value 98
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
