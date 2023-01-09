7fc4aa51e351eff777eeccc6144a608c702e6988
nathan-assefa <nnathanassefa@gmail.com>
2023-01-09 01:58:30 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,8 @@
 the an object is an instance of a supper or base
 class. Otherwise this function returns false
 """
+
+
 def is_kind_of_class(obj, a_class):
     """obj is an object that inherits from
     a_class, and this function checks if obj_j
