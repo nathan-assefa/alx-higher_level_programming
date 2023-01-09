ec4f9267f8789a80cb8123d92a9337e81a96bde3
nathan-assefa <nnathanassefa@gmail.com>
2023-01-09 04:25:22 -0800

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""The new class Rectangle inherits from the old
+class BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inherits from the parent class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {}".format(width/height)
+
+    def area():
+        return self.__width * self.__height
