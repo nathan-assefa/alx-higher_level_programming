cb41abb10bbf53477c704e9ac9bca88df79165b5
nathan-assefa <nnathanassefa@gmail.com>
2023-01-09 04:50:50 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,12 +5,11 @@ class BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-
 class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-        
+
     def area(self):
         return (self.__size**2)
