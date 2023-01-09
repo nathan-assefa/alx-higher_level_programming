f0586b4a685a1880b24aee228b90a7550f728075
nathan-assefa <nnathanassefa@gmail.com>
2023-01-09 06:40:29 -0800

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this function is to add new attribute to an
+object if possible. if not possible, the functoin
+raises an exception
+"""
+
+
+
+def add_attribute(obj, attribute, value):
+    """to add new atributes to an object"""
+    print(obj)
+    if isinstance(obj, type):
+        raise TypeError("[TypeError] can't add new attribute")
+    print(id(obj))
+    obj.attribute = valu
