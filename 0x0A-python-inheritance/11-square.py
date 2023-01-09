8a6f3e0ac9f231715114b4e9e80f3da9eceb917f
nathan-assefa <nnathanassefa@gmail.com>
2023-01-09 05:04:57 -0800

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""The new class Rectangle inherits from the old
+class BaseGeometry
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Inherits from Rectangle class and prints area
+    using a method from another class
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+        def __str__(self):
+            return "[Square] {}/{}".format(self.__size, self.__size)
