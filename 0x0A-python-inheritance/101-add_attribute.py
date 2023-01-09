1872a744342cbab88d0451af15806c4e8e77f6c6
nathan-assefa <nnathanassefa@gmail.com>
2023-01-09 06:51:03 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,8 +8,8 @@ raises an exception
 def add_attribute(obj, name, value):
     """Method checking if attribute can be set and sets
     where possible"""
-    if hasattr(obj, "__dict__") or \
-        (hasattr(obj, "__slots__") and name in obj.__slots__):
+    if hasattr(obj, "__dict__") or (
+        hasattr(obj, "__slots__") and name in obj.__slots__):
         setattr(obj, name, value)
     else:
         raise TypeError("can't add new attribute")
