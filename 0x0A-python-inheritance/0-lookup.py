ab2d0739faca30731d0e74855aa96f1165c19475
nathan-assefa <nnathanassefa@gmail.com>
2023-01-09 01:25:51 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""This functioin prints all the
+attributes whithin an object
+"""
+
 
 def lookup(obj):
+    """Obj is an object whose attributes
+    will be displayed by this function
+    """
     print(dir(obj))
