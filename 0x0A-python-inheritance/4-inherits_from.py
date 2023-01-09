0f5eda0c8bda475b14e9b403d5f59596555c3891
nathan-assefa <nnathanassefa@gmail.com>
2023-01-09 02:33:23 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ from the specified class ; otherwise False.
 
 def inherits_from(obj, a_class):
     """Returns True of Flase depednig on the object type"""
-     return False if type(obj) is a_class else isinstance(obj, a_class)
+    return False if type(obj) is a_class else isinstance(obj, a_class)
