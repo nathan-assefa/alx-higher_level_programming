1a4ad87b78dfa8448dbfcf3ad93a884b1199c92d
nathan-assefa <nnathanassefa@gmail.com>
2023-01-10 01:22:55 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ the content to stdout
 def read_file(filename=""):
     """open fucntion opens the file in read mode"""
     with open('filename') as file:
-        print("{}".format(file.read()))
+        print("{}".format(file.read()), end="")
