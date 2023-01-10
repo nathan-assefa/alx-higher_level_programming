7a4e973c52d4da93523a445d1da4e4233cb51cd4
nathan-assefa <nnathanassefa@gmail.com>
2023-01-10 06:23:47 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This functioin adds arguments to python list
+and write them into the json file
+"""
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    jsonList = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    jsonList = []
+
+for item in argv[1:]:
+    jsonList.append(item)
+
+save_to_json_file(jsonList, 'add_item.json')
