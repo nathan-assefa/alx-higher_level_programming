7dc30a379da7758ee2ae923355428428d4ac0eab
nathan-assefa <nnathanassefa@gmail.com>
2023-01-10 02:14:51 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This function returns python data structure
+form json file
+"""
+
+
+def from_json_string(my_str):
+    """Retuns python data structure"""
+    return json.loads(my_str)
