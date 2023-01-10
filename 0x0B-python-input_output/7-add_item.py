4558c18c60eda2ee4e93a93bc77dddcf30679f9d
nathan-assefa <nnathanassefa@gmail.com>
2023-01-10 05:41:27 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 """This functioin adds arguments to python list
 and write them into the json file
 """
-import sys
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
