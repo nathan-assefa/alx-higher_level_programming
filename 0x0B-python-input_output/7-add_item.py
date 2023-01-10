a1471f75d05ffdc2d05182acffca97c6044f1115
nathan-assefa <nnathanassefa@gmail.com>
2023-01-10 05:37:18 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,12 @@ and write them into the json file
 """
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys
 
 
+try:
+    jsonList = load_from_json_file('add_item.json')
+except:
+    jsonList = []
 
+save_to_json_file(jsonList.append(argv[:]))
