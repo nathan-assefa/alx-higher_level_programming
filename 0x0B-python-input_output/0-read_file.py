34ad55facf28a3640e1d2d8c51f3ab151005a9ca
nathan-assefa <nnathanassefa@gmail.com>
2023-01-10 01:02:40 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This function reads from a file and prints
+the content to stdout
+"""
+
+
+def read_file(filename=""):
+    """open fucntion opens the file in read mode"""
+    with open("filename", encoding="utf-8") as file:
