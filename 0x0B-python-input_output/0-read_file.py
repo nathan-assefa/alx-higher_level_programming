8f960fa1fd1fcd6be8367054c7961ba49ad06e34
nathan-assefa <nnathanassefa@gmail.com>
2023-01-10 01:35:47 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ the content to stdout
 def read_file(filename=""):
     """open fucntion opens the file in read mode"""
     with open(filename, encoding='utf-8') as file:
-        for f in file:
-            print("{}".format(f), end="")
+        print(file.read().rstrip())
