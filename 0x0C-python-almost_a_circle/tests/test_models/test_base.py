b48052c85de4d13b95e3b4c28aefd5a4ee31a39e
nathan-assefa <nnathanassefa@gmail.com>
2023-01-17 06:26:44 -0800

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -110,31 +110,31 @@ class TestBase_save_to_file(unittest.TestCase):
         except IOError:
             pass
 
-    def test_save_to_file_one_rectangle(self):
-        r = Rectangle(10, 7, 2, 8, 5)
-        Rectangle.save_to_file([r])
-        with open("Rectangle.json", "r") as f:
-            self.assertTrue(len(f.read()) == 53)
+    def test_IfTheFunctionExist(self):
+        """To check if from_json_string method exist"""
+        self.assertTrue(Base.from_json_string)
 
-    def test_save_to_file_two_rectangles(self):
+    def test_TheRightOutput(self):
+        """Chech the result of the method"""
+        a = Rectangle(2, 4, 9, 4, 1)
+        Rectangle.save_to_file([a])
+        with open("Rectangle.json") as file:
+            self.assertEqual(len(file.read()), 53)
+
+    def test_IfNewFileExist(self):
+        """Check a file created by the save_to_file method exists"""
         r1 = Rectangle(10, 7, 2, 8, 5)
-        r2 = Rectangle(2, 4, 5, 9)
+        r2 = Rectangle(2, 4, 5, 6, 9)
         Rectangle.save_to_file([r1, r2])
-        with open("Rectangle.json", "r") as f:
+        with open("Rectangle.json") as f:
             self.assertEqual(len(f.read()), 105)
 
-    def test_save_to_file_one_square(self):
-        s = Square(10, 7, 2, 8)
-        Square.save_to_file([s])
-        with open("Square.json", "r") as f:
-            self.assertTrue(len(f.read()) == 39)
-
-    def test_save_to_file_two_squares(self):
-        s1 = Square(10, 7, 2, 8)
-        s2 = Square(8, 1, 2, 3)
-        Square.save_to_file([s1, s2])
-        with open("Square.json", "r") as f:
-            self.assertTrue(len(f.read()) == 77)
+    def test_TheRightOutput(self):
+        """Chech the result of the method"""
+        a = Square(2, 5, 9, 8)
+        Square.save_to_file([a])
+        with open("Square.json") as file:
+            self.assertEqual(len(file.read()), 38)
 
     def test_save_to_file_cls_name_for_filename(self):
         s = Square(10, 7, 2, 8)
