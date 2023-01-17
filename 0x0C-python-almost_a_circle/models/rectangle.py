f23fa8985c4b238e13311493ef6d682161b06aa6
nathan-assefa <nnathanassefa@gmail.com>
2023-01-16 22:09:53 -0800

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,19 @@ class Rectangle(Base):
     """intializing a new instance using init method"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+            Raises:
+                TypeError: If either of width or height is not an int.
+                ValueError: If either of width or height <= 0.
+                TypeError: If either of x or y is not an int.
+                ValueError: If either of x or y < 0.
+        """
         self.width = width
         self.height = height
         self.x = x
@@ -43,37 +56,45 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Getting width attribute"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setting a value to width attribure"""
         self.validation("width", value)
         self.__width = value
 
     @property
     def height(self):
+        """Getting height attribute"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setting a value for height attribute"""
         self.validation("height", value)
         self.__height = value
 
     @property
     def x(self):
+        """Getting x attribute"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """Setting a value for x attribute"""
         self.validation("x", value)
         self.__x = value
 
     @property
     def y(self):
+        """Getting y attribute"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Setting a value for y attribute"""
         self.validation("y", value)
         self.__y = value
 
