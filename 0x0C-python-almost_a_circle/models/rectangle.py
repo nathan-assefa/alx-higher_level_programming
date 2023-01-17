267f5aa6b07f1612014216b88e56a1c94797fffa
nathan-assefa <nnathanassefa@gmail.com>
2023-01-16 21:59:47 -0800

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,154 +1,137 @@
 #!/usr/bin/python3
-"""Defines a rectangle class."""
-from models.base import Base
+Base = __import__("Base").Base
+"""The Base class is the parent class of
+the class Rectangle, and the Rectangle class
+inherits the methods and attributs of the Base
+class using super() method
+Args:
+    width: width of a structure
+    height: height of a structure
+    x, and y are the two paramets detrmines the
+    the extra features of the structure.
+    id: This attribute is from the parent class and
+    its vaue is detirmined by its state.
+There are also different methods in this Class used to
+set up a structure.
+"""
 
 
 class Rectangle(Base):
-    """Represent a rectangle."""
+    """intializing a new instance using init method"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
-        Raises:
-            TypeError: If either of width or height is not an int.
-            ValueError: If either of width or height <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
-        """
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
 
+    def __str__(self):
+        """str method to print an object when print is used"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x, self.__y, self.__width, self.__height
+                )
+
+    def validation(self, key, val):
+        """This function validats the data"""
+        if not isinstance(val, int):
+            raise TypeError("{} must be an integer".format(key))
+        if (key == "width" or key == "height") and val <= 0:
+            raise ValueError("{} must be an integer".format(key))
+        if (key == "x" or key == "y") and val < 0:
+            raise ValueError("{} must be >= 0".format(key))
+
     @property
     def width(self):
-        """Set/get the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        self.validation("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """Set/get the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
+        self.validation("height", value)
         self.__height = value
 
     @property
     def x(self):
-        """Set/get the x coordinate of the Rectangle."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        self.validation("x", value)
         self.__x = value
 
     @property
     def y(self):
-        """Set/get the y coordinate of the Rectangle."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+        self.validation("y", value)
         self.__y = value
 
     def area(self):
-        """Return the area of the Rectangle."""
-        return self.width * self.height
+        """Area method to find the area"""
+        return self.__width * self.__height
 
     def display(self):
-        """Print the Rectangle using the `#` character."""
-        if self.width == 0 or self.height == 0:
-            print("")
-            return
-
-        [print("") for y in range(self.y)]
-        for h in range(self.height):
-            [print(" ", end="") for x in range(self.x)]
-            [print("#", end="") for w in range(self.width)]
-            print("")
+        """To display the structure"""
+        if self.__y > 0:
+            [print() for i in range(self.__y)]
+        for i in range(self.__height):
+            [print(" ", end="") for k in range(self.__x)]
+            [print("#", end="") for j in range(self.__width)]
+            print()
 
     def update(self, *args, **kwargs):
-        """Update the Rectangle.
-
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents width attribute
-                - 3rd argument represent height attribute
-                - 4th argument represents x attribute
-                - 5th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
+        """This function is used to intialize the attributes
+        of a new instance. When this function is called, then
+        the attributes of a new instance would be intialized
         """
         if args:
-            a = 0
+            """This is for the new non key parametrs"""
+            x = 0
             for arg in args:
-                if a == 0:
+                if x == 0:
                     if arg is not None:
                         self.id = arg
-                elif a == 1:
+                elif x == 1:
                     self.width = arg
-                elif a == 2:
+                elif x == 2:
                     self.height = arg
-                elif a == 3:
+                elif x == 3:
                     self.x = arg
-                elif a == 4:
+                elif x == 4:
                     self.y = arg
-                a += 1
-
-        elif kwargs:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is not None:
-                        self.id = v
-                elif k == "width":
-                    self.width = v
-                elif k == "height":
-                    self.height = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+                x += 1
+        elif kwargs and len(kwargs) != 0:
+            """This is for key worded parametrs"""
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is not None:
+                        self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
     def to_dictionary(self):
-        """Return the dictionary representation of a Rectangle."""
+        """To convert to dictionary"""
         return {
-            "id": self.id,
-            "width": self.width,
-            "height": self.height,
-            "x": self.x,
-            "y": self.y
-        }
-
-    def __str__(self):
-        """Return the print() and str() representation of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x, self.y,
-                                                       self.width, self.height)
+                "x": self.x,
+                "y": self.y,
+                "id": self.id,
+                "height": self.height,
+                "width": self.width,
+                }
