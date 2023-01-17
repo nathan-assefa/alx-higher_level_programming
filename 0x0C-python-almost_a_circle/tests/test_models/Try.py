98ff031dd3be37663aa3518275c36872229acddd
nathan-assefa <nnathanassefa@gmail.com>
2023-01-17 04:52:39 -0800

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/tests/test_models/Try.py
+++ 0x0C-python-almost_a_circle/tests/test_models/Try.py
@@ -56,47 +56,39 @@ class TestBase_instantiation(unittest.TestCase):
 class TestBase_to_json_string(unittest.TestCase):
     """Unittests for testing to_json_string method of Base class."""
 
-    def test_to_json_string_rectangle_type(self):
-        r = Rectangle(10, 7, 2, 8, 6)
-        self.assertEqual(str, type(Base.to_json_string([r.to_dictionary()])))
+    def test_IfTheMethodExist(self):
+        """check if the method exist in the Base Class"""
+        self.assertTrue(Base.to_json_string)
 
-    def test_to_json_string_rectangle_one_dict(self):
-        r = Rectangle(10, 7, 2, 8, 6)
-        self.assertTrue(len(Base.to_json_string([r.to_dictionary()])) == 53)
+    def test_IfTheMethodWorks(self):
+        """check if the method works in the manner"""
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual("<class 'str'>", str(type(json_dictionary)))
 
-    def test_to_json_string_rectangle_two_dicts(self):
-        r1 = Rectangle(2, 3, 5, 19, 2)
-        r2 = Rectangle(4, 2, 4, 1, 12)
-        list_dicts = [r1.to_dictionary(), r2.to_dictionary()]
-        self.assertTrue(len(Base.to_json_string(list_dicts)) == 106)
+    def test_ReturnValue(self):
+        """Check if the method returns the right vlaue"""
+        js_string = Base.to_json_string([{"name": "Nathan"}])
+        self.assertEqual('[{"name": "Nathan"}]', js_string)
 
-    def test_to_json_string_square_type(self):
-        s = Square(10, 2, 3, 4)
-        self.assertEqual(str, type(Base.to_json_string([s.to_dictionary()])))
+    def test_ForEmptyObject(self):
+        """Check when the object is empty or None"""
+        js_dict = Base.to_json_string(None)
+        self.assertEqual("[]", js_dict)
 
-    def test_to_json_string_square_one_dict(self):
-        s = Square(10, 2, 3, 4)
-        self.assertTrue(len(Base.to_json_string([s.to_dictionary()])) == 39)
+        js_dict = Base.to_json_string([])
+        self.assertEqual("[]", js_dict)
 
-    def test_to_json_string_square_two_dicts(self):
-        s1 = Square(10, 2, 3, 4)
-        s2 = Square(4, 5, 21, 2)
-        list_dicts = [s1.to_dictionary(), s2.to_dictionary()]
-        self.assertTrue(len(Base.to_json_string(list_dicts)) == 78)
+    def test_PassingInvalidInput(self):
+        """check if the input data is invalid"""
+        with self.assertRaises(TypeError):
+            Base.to_json_string([{"id": 3}], 2)
 
-    def test_to_json_string_empty_list(self):
-        self.assertEqual("[]", Base.to_json_string([]))
-
-    def test_to_json_string_none(self):
-        self.assertEqual("[]", Base.to_json_string(None))
-
-    def test_to_json_string_no_args(self):
+    def test_IfNoParameterIsPassed(self):
+        """When ther is no argument"""
         with self.assertRaises(TypeError):
             Base.to_json_string()
-
-    def test_to_json_string_more_than_one_arg(self):
-        with self.assertRaises(TypeError):
-            Base.to_json_string([], 1)
 
 
 class TestBase_save_to_file(unittest.TestCase):
