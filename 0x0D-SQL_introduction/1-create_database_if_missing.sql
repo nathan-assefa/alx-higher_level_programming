a3f8958c3e298f05075db86bcf1fa34381406f43
nathan-assefa <nnathanassefa@gmail.com>
2023-02-14 01:40:21 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
---This script creates a database
---Does not falil if the database already exists
+-- This script creates a database
+-- Does not falil if the database already exists
 CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
