15045ee20f80f229b06c6ac0d73d6bdc74417793
nathan-assefa <nnathanassefa@gmail.com>
2023-02-14 03:57:45 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,2 @@
 -- Creates tables and adds muliples of records inti
-CREATE TABLE second_table (`id` INT, `name` VARCHAR(256), `score` INT);
+CREATE TABLE IF NOT EXISTS second_table (`id` INT, `name` VARCHAR(256), `score` INT);
