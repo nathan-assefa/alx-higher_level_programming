4bcec488e042f56d4f38b542cfdff2c4ebaaea77
nathan-assefa <nnathanassefa@gmail.com>
2023-02-14 04:09:09 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,6 @@
 -- Creates tables and adds muliples of records inti
 CREATE TABLE IF NOT EXISTS second_table (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO second_table (id, name) VALUES (1, 10)
-INSERT INTO second_table (id, name) VALUES (2, 3)
-INSERT INTO second_table (id, name) VALUES (3, 14)
-INSERT INTO second_table (id, name) VALUES (4, 82, 3)
-INSERT INTO second_table (id, name) VALUES (3, 14)
-INSERT INTO second_table (id, name) VALUES (4, 8))
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
