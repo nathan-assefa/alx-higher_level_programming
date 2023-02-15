9cb3f5a93b9f9f1d4008c093c848133979b18364
nathan-assefa <nnathanassefa@gmail.com>
2023-02-15 01:48:27 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Creating a new user in our data base
-CREATE USER 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'IF NOT EXISTS';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
