3e8234d5ed92e8557c16d3fb807f5ca906144f7e
nathan-assefa <nnathanassefa@gmail.com>
2023-02-15 05:34:31 -0800

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows
+-- +that does not have genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id = NULL  ORDER BY tv_shows.title, tv_show_genres.genre_id;
