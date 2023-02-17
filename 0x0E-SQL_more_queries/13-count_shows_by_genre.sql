b74c9b70265e864186b39045c2ffa2c6b10bd41c
nathan-assefa <nnathanassefa@gmail.com>
2023-02-16 23:48:43 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,5 @@ SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.show_id) AS 'number_of_sh
 FROM tv_genres INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
+GROUP BY tv_show_genres.show_id
 ORDER BY number_of_shows DESC;
