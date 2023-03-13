c51c3c6a4b306619b84c832edfb8f76ae2302121
nathan-assefa <nnathanassefa@gmail.com>
2023-03-13 04:48:08 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-let ans = 0;
-num = parseInt(process.argv[2]);
-function factorial(num) {
-  if(num === 1) {
+
+const num = parseInt(process.argv[2]);
+function factorial (n) {
+  if (n === 1 || isNaN(n)) {
     return (1);
-  } ans += factorial(num - 1);
+  } return (n * factorial(n - 1));
 }
+console.log(factorial(num));
