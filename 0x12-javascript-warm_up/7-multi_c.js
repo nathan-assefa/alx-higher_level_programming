e4bd2a9aa327e9b243d1c366c53bf4fb77dfef4d
nathan-assefa <nnathanassefa@gmail.com>
2023-03-13 03:52:25 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 
 if (isNaN(process.argv[2]) {
   console.log('Missing number of occurrences');
-else {
-	for (let i = process.argv[2]; i > 0; i--) {
-	  console.log('C is fun');
-	}
+} else {
+  for (let i = parseInt(process.argv[2]); i > 0; i--) {
+    console.log('C is fun');
+  }
 }
