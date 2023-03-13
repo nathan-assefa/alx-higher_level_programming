a458cafb40b2f86d41a73ea56912510988e82713
nathan-assefa <nnathanassefa@gmail.com>
2023-03-13 07:17:27 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
 myObject.incr = function () {
-  myObject.value++;
+  this.value++;
 };
 myObject.incr();
 console.log(myObject);
