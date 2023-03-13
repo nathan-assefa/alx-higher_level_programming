a5a392bbfd843f73dd5eff8a43f43b599a552cc1
nathan-assefa <nnathanassefa@gmail.com>
2023-03-13 04:00:41 -0700

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const X = process.argv[2];
+if (isNaN(X) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < X; i++) {
+    for (let j = 0; j < X; i++) {
+      consile.log(X);
+    }
+    console.log();
+  }
+}
