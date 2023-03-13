d8d0abf1220cd1c97d0cfeeaab8d49bd7a2deb0f
nathan-assefa <nnathanassefa@gmail.com>
2023-03-13 04:11:04 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
 const X = process.argv[2];
-if (isNaN(X) {
+if (isNaN(X)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < X; i++) {
-    for (let j = 0; j < X; i++) {
-      consile.log(X);
-    }
-    console.log();
+  for (let i = 0; i < parseInt(X); i++) {
+    console.log('X'.repeat(parseInt(X)));
   }
 }
