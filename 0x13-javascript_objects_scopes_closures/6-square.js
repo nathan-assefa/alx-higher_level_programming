58269ae87d940cd62d049ea7a97b6f3b003e43b6
nathan-assefa <nnathanassefa@gmail.com>
2023-03-14 08:16:43 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
-
-const Rectangle = require('./5-square.js');
-
-class Square extends Rectangle {
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
-    }
-    else {
-      for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
-      }
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
-}
-
-module.exports = Square;
+};
