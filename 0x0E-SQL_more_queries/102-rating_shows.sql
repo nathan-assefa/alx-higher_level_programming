d04d3196ef703e089ba693d9376da02f341908b9
nathan-assefa <nnathanassefa@gmail.com>
2023-03-15 04:55:06 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,5 +3,5 @@
 SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY title
+GROUP BY tv_show_ratings.show_id
 ORDER BY rating DESC;
