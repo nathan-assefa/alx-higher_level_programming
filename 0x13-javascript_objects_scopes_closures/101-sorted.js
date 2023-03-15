6542b8a4ea1ecfd9aef3140e4351d1f8fe653fb7
nathan-assefa <nnathanassefa@gmail.com>
2023-03-15 02:56:48 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,10 @@
 const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
+  if (newDict[dict[key]]) {
     newDict[dict[key]].push(key);
+  } else {
+    newDict[dict[key]] = [key];
   }
 }
 console.log(newDict);
