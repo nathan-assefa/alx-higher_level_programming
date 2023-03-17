d3d233f36af86c1f8d160ca829e10e27ef9d2800
nathan-assefa <nnathanassefa@gmail.com>
2023-03-17 12:25:22 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,16 @@
 + the data base"""
 
 
-from sys import argv
-import MySQLdb
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
 
-db = MySQLdb.connect(
-    host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-)
-cr = db.cursor()
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
+    cr = db.cursor()
 
-cr.execute("SELECT states.id, states.name FROM states")
-[print(state) for state in cr.fetchall()]
-cr.close()
-db.close()
+    cr.execute("SELECT states.id, states.name FROM states")
+    [print(state) for state in cr.fetchall()]
+    cr.close()
+    db.close()
