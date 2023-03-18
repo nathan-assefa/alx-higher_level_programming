97318694bdfe5c5a8ce9704aaf2ce99c4042fd91
nathan-assefa <nnathanassefa@gmail.com>
2023-03-18 04:45:47 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """ This script defines the satate object in the database
 using ORM"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
 
 
-if __name__ == "__main__":
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy import Column, Integer, String
+class State(Base):
+    """This class defines the methadata for the table 'states'"""
 
-    Base = declarative_base()
-    class State(Base):
-        """This class defines the methadata for the table 'states'"""
+    __tablename__ = 'states'
 
-        __tablename__ = 'states'
-
-        id = Column(Integer, primary_key=true, unique=true)
-        name = Column(String(30), nullable=false)
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(30), nullable=False)
         
 
