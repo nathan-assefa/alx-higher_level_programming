879fd799aafe814f648f6cf66b821352aea15f5a
nathan-assefa <nnathanassefa@gmail.com>
2023-03-18 05:44:21 -0700

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This script prints all the states starting from letter 'a'
+from the database using ORM tecnique"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    s = session.query(State).filter(State.name == sys.argv[4]).all()
+    if s:
+        for state in s:
+            print("{}".format(state.id))
+    else:
+        print("Not found" )
