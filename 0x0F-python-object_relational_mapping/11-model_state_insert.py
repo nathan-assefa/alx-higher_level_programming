0db6ad4e15d50a4b131b37089aa8e34c6773b5f0
nathan-assefa <nnathanassefa@gmail.com>
2023-03-18 05:52:33 -0700

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This script prints all the states starting from letter 'a'
+from the database using ORM tecnique"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(State(name='Louisiana'))
+    session.commit()
