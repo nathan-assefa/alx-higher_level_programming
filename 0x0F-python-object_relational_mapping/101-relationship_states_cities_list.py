5f6896928f551788ee75efa2e808f0ce489b086f
nathan-assefa <nnathanassefa@gmail.com>
2023-03-19 12:05:13 -0700

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This script adds a city in from the state class
+using the relation"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).all()
+    for s in state:
+        print("{}: {}".format(s.id, s.name))
+        [print("\t{}: {}".format(city.id, city.name)) for city in s.cities]
+    session.commit()
