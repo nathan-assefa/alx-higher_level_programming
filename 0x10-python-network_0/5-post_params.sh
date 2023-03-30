927f4f84104f397f3152f68adaa69cbedf31245c
nathan-assefa <nnathanassefa@gmail.com>
2023-03-30 04:05:54 -0700

    0x10-pyhton-network_0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script POST request to the passed URL, and displays the body of the response
-curl -s -L -d '{"email": "test@gmail.com", "variable": "I will always be here for PLD"}' -X POST "$1" 
+curl -s -L -d '{"email"="test@gmail.com", "variable"="I will always be here for PLD"}' -X POST "$1" 
