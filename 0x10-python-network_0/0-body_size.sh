cd33d351d2695f287cf8b04bea5d35de4323133c
nathan-assefa <nnathanassefa@gmail.com>
2023-03-30 02:15:08 -0700

    0x10-pyhton-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This gets the size of the http reponse in byte
 
-curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' ' | wc -c
