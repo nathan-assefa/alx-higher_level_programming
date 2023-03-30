4eccc854854e4dff834be76d4bc2efcf176bbc76
nathan-assefa <nnathanassefa@gmail.com>
2023-03-30 05:50:27 -0700

    0x10-pyhton-network_0

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script isplays only the status code of the response form the requested URL
-curl -L -s -X HEAD --write-out "%{http_code}" "$1"
+curl -s -X HEAD --write-out "%{http_code}" "$1"
