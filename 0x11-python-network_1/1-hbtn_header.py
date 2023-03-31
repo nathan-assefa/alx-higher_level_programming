8099c70ec10b449d03c081e1122d6bc198b92e78
nathan-assefa <nnathanassefa@gmail.com>
2023-03-31 02:29:17 -0700

    0x11-python-network_1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,4 +8,9 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    
+    with urllib.request.urlopen(argv[1]) as res:
+        # since the 'get' method only work with dictionary, we first need to change the headers into dict
+        print(res.headers.__dict__.get('X-Request-Id'))
+
+        # here we can also use getheader method wich recieves the key and returns the value
+        # print(res.getheader('X-Request-I'))
