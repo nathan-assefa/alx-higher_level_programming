6f72d881d3faaa2ec1da36cdf6cfd63c19f66650
nathan-assefa <nnathanassefa@gmail.com>
2023-03-31 06:56:54 -0700

    0x11-python-network_1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 
+    sends a request to the URL and displays the body
+    of the response (decoded in utf-8).
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    """ Displaying a response and handling erros """
+    r = requests.get(argv[1])
+    if r.status_code != 200:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
