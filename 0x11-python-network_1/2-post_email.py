1b7928b6a8328ffd6ae82d52e7f73fbf383c4ca8
nathan-assefa <nnathanassefa@gmail.com>
2023-03-31 04:45:23 -0700

    0x11-python-network_1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,21 +2,24 @@
 """ This script sends some data to a web server using urllib module """
 
 
-if __name__ == "__mani__":
+if __name__ == "__main__":
     """ Encoding the data to be sent and make a request """
-    import urllib.request
-    import urllib.parse
+    from urllib import request, parse
     from sys import argv
 
     # First write the query string as a dictionary
     data = {'email': argv[2]}
 
     # Then the data has to be encoded into standard way
-    encoded_data = data.parse.urlencode(data)
+    encoded_data = parse.urlencode(data)
 
     # Thenafter, the data should be bytes, and for that we use one more
     # +method called encod('ascii')
-    final_data = encoded_data.encode('ascii') # finally we have standard data
+    final_data = encoded_data.encode('ascii')
+    # finally we have standard data
 
-    with urllib.request.urlopen(argv[1], final_data) as res:
+    # Here we request a URL
+    req = request.Request(argv[1], final_data)
+
+    with request.urlopen(req) as res:
         print(res.read().decode('utf-8'))
