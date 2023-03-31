28ae3f8db25c7a217eb9016140c944456fd33cab
nathan-assefa <nnathanassefa@gmail.com>
2023-03-31 05:55:21 -0700

    0x11-python-network_1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This script sends some data to a web server using requests module """
+
+
+if __name__ == "__main__":
+    """ Since we use requests module, the data would be
+    encoded automatically """
+
+    import requests
+    from sys import argv
+
+    requests.post(argv[1], data={'email': argv[2]})
