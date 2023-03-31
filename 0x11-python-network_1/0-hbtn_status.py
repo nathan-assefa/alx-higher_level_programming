1cbfc2694ccbecfcebf7bef2e8b736ad5c28f396
nathan-assefa <nnathanassefa@gmail.com>
2023-03-31 02:08:09 -0700

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
-        #decoding the bytes into python string using decode method
+        # decoding the bytes into python string using decode method
         _str = response.decode('utf-8')
         print("\t- utf8 content: {}".format(_str))
