9ec1a497bafe29bf0ccfac48ad6b80cc7560fde3
nathan-assefa <nnathanassefa@gmail.com>
2023-03-31 03:31:08 -0700

    0x11-python-network_1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import urllib.request
-from sys import argv
 """
     Sends request to URL and displays the value of the
     X-Request-Id variable found in the header of the response.
@@ -8,6 +6,10 @@ from sys import argv
 
 
 if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
+    """ This code fetches the data in a variable """
+
     with urllib.request.urlopen(argv[1]) as res:
         # since the 'get' method only work with dictionary, we first need to change the headers into dict
         print(res.headers.__dict__.get('X-Request-Id'))
