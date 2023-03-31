8dcd6eb5d30ea24b787dd01a03233bcee4c46029
nathan-assefa <nnathanassefa@gmail.com>
2023-03-31 03:28:43 -0700

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,12 @@ import urllib.request
 
 if __name__ == "__main__":
     """ Fetching data from URL """
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         response = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
-
+        # decoding the bytes into python string using decode method
         _str = response.decode('utf-8')
         print("\t- utf8 content: {}".format(_str))
