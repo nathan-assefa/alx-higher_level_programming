a9bfb88af41867c6c9c67dbebd98477b24532a9a
nathan-assefa <nnathanassefa@gmail.com>
2023-03-31 02:07:07 -0700

    0x11-python-network_1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import urllib.request
+from sys import argv
+"""
+    Sends request to URL and displays the value of the
+    X-Request-Id variable found in the header of the response.
+"""
+
+
+if __name__ == "__main__":
+    
