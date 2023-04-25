b453991975e564a8bff8cb47b9287aa89744d6fe
nathan-assefa <nnathanassefa@gmail.com>
2023-04-25 13:17:51 -0700

    0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+const request = require('request')
+const url = process.argv[2]
+
+request(url, (error, body) => {
+	if (error) throw new Error(error)
+	const data = JSON.stringify(body)
+	fs.writeFile(process.argv[3], data, 'utf8', (err) => {
+		if (err) throw new Error(err)
+	})
+})
