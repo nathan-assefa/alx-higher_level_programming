3626186a025c0beafcf7c4838dd749f7b8cc7b78
nathan-assefa <nnathanassefa@gmail.com>
2023-04-25 12:43:47 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,24 +1,26 @@
 #!/usr/bin/node
-const request = require('request-promise');
+const request = require('request');
+
+function printCharacters(characters, index) {
+  if (index === characters.length) {
+    return;
+  }
+  request(characters[index], function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log(JSON.parse(body).name);
+    } else {
+      console.error('Failed to get character:', characters[index]);
+    }
+    printCharacters(characters, index + 1);
+  });
+}
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-request(url)
-  .then((body) => {
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
-    const promises = characters.map((charUrl) => {
-      return request(charUrl).then((charBody) => {
-        const name = JSON.parse(charBody).name;
-        return name;
-      });
-    });
-    return Promise.all(promises);
-  })
-  .then((names) => {
-    names.forEach((name) => {
-      console.log(name);
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+    printCharacters(characters, 0);
+  } else {
+    console.error('Failed to get movie:', process.argv[2]);
+  }
+});
