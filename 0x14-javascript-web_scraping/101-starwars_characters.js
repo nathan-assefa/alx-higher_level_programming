512212e9a4eced0c104b6ce3f905e5b77a43b96a
nathan-assefa <nnathanassefa@gmail.com>
2023-04-25 12:23:33 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,33 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(url, (error, response, body) => {
-  if (error) throw new Error(error);
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-  const resp = JSON.parse(body).characters;
-  resp.forEach((charURL) => {
-    request(charURL, (error, response, body) => {
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+  const characterNames = [];
+
+  function printCharacterNames() {
+    if (characterNames.length === characters.length) {
+      characterNames.forEach((name) => console.log(name));
+    }
+  }
+
+  characters.forEach((url) => {
+    request(url, function (error, response, body) {
       if (error) {
         console.error(error);
         return;
       }
-
-      const character = JSON.parse(body).name;
-      console.log(character);
+      const character = JSON.parse(body);
+      characterNames[characters.indexOf(url)] = character.name;
+      printCharacterNames();
     });
   });
 });
