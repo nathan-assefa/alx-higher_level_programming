721b6df068b1524141fe05a26d5ef3d1384bd61f
nathan-assefa <nnathanassefa@gmail.com>
2023-04-25 12:53:52 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 
-function printCharacters(characters, index) {
+function printCharacters (characters, index) {
   if (index === characters.length) {
     return;
   }
   request(characters[index], function (error, response, body) {
-    if (!error && response.statusCode === 200) {
+    if (!error) {
       console.log(JSON.parse(body).name);
-    } else {
-      console.error('Failed to get character:', characters[index]);
     }
     printCharacters(characters, index + 1);
   });
@@ -17,10 +15,8 @@ function printCharacters(characters, index) {
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+  if (!error) {
     const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
-  } else {
-    console.error('Failed to get movie:', process.argv[2]);
   }
 });
